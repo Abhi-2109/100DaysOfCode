4ca851574233a9b86ef6c62c85ad1986ae60d3d5
Prakhar Sharan <35136020+itsmeprakhar22@users.noreply.github.com>
2018-11-04 01:23:02 -0500

    Update and rename bb.cpp to balanced_bracket.cpp

--- balanced_bracket.cpp
+++ balanced_bracket.cpp
@@ -21,15 +21,19 @@ int main()
     {
       case '{':
       case  '(':
-      case '[': s1.push(ch);
+      case '[': s1.push(ch);  //push the opening brackets on the stack
       break;
+        //For closing brackets
+        //Ensure that the stack is not empty as opening bracket is must for closing bracket
+        //if the top of stack contains the matching opening bracket then pop it
+        //else mark as unbalanced (f=1)
       case '}':
         if(s1.empty())
-        f=1;
+        f=1;  //mark unbalanced if stack is already empty
         else if(s1.top()=='{')
-        s1.pop();
+        s1.pop(); //pop the matching opening bracket
         else
-        f=1;
+        f=1;  //mark unbalanced
         break;
       case ')':
         if(s1.empty())
